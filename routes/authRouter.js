b4955d926b0ebf7a2f68d862b1026022d6cd2c66
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2025-02-16 12:21:36 -0500

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,12 +4,14 @@ const passport = require('passport')
 const jwt = require('jsonwebtoken')
 const config = require('./../config/config')
 const UserService = require('./../services/userService')
-const service = new UserService()
+const AuthService = require('./../services/authService')
+const serviceUser = new UserService()
+const serviceAuth = new AuthService()
 
 router.post('/register', async (req, res, next) => {
     try {
         const newUser = req.body
-        const response = await service.createUser(newUser)
+        const response = await serviceUser.createUser(newUser)
         delete response.dataValues.password
         res.status(201).json(response)
     } catch (err) {
@@ -23,15 +25,8 @@ router.post('/login',
     try {
         const user = req.user
         delete user.dataValues.password
-        const payload = {
-            sub: user.id,
-            role: user.role
-        }
-        const token = jwt.sign(payload, config.jwtSecret)
-        res.json({
-            user,
-            token
-        })
+        const token = await serviceAuth.login(user)
+        res.status(201).json(token)
     } catch (err) {
         next(err)
     }
@@ -42,12 +37,32 @@ router.get('/profile',
     async (req, res, next) => {
         try {
             const user = req.user
-            const perfil = await service.getUserForId(user.sub)
+            const perfil = await serviceUser.getUserForId(user.sub)
             delete perfil.dataValues.password
             res.json(perfil)
         } catch (err) {
             next(err)
         }
     })
+
+router.post('/recovery', async (req, res, next) => {
+    try {
+        const { email } = req.body
+        const response = await serviceAuth.sendMail(email)
+        res.json(response)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/change-password', async (req, res, next) => {
+    try {
+        const { token, newPassword } = req.body
+        const response = await serviceAuth.changePassword(token, newPassword)
+        res.json(response)
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router
