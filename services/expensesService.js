0c317708ce96bf132e7d76bcc6da39d77dd53e00
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2025-02-17 18:04:09 -0500

    Create expensesService.js

---
+++ services/expensesService.js
@@ -0,0 +1,64 @@
+const boom = require('@hapi/boom')
+const { models } = require('./../libs/sequelize')
+
+class ExpensesService {
+    async createExpense(expense) {
+        const newExpense = await models.Expenses.create(expense)
+        if (!newExpense) {
+            throw boom.notFound('No se pudo crear el gasto')
+        }
+        return newExpense
+    }
+
+    async getExpenses(userId) {
+        const expenses = await models.Expenses.findAll({
+            where: {
+                userId
+            }
+        })
+        return expenses
+    }
+
+    async getExpense(userId, id) {
+        const expense = await models.Expenses.findOne({
+            where: {
+                userId,
+                id
+            }
+        })
+        if (!expense) {
+            throw boom.notFound('No se encontro el gasto')
+        }
+        return expense
+    }
+
+    async updateExpense(userId, id, changes) {
+        const expense = await models.Expenses.findOne({
+            where: {
+                userId,
+                id
+            }
+        })
+        if (!expense) {
+            throw boom.notFound('No se encontro el gasto')
+        }
+        await expense.update(changes)
+        return expense
+    }
+
+    async deleteExpense(userId, id) {
+        const expense = await models.Expenses.findOne({
+            where: {
+                userId,
+                id
+            }
+        })
+        if (!expense) {
+            throw boom.notFound('No se encontro el gasto')
+        }
+        await expense.destroy()
+        return { message: 'El gasto se elimino correctamente' }
+    }
+}
+
+module.exports = ExpensesService
