c20af336fdc80d8756c5e2a2fe2e984dbbc1e010
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2025-02-17 17:50:08 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const userRouter = require('./userRouter')
 const proyectRouter = require('./proyectsRouter')
 const folderRouter = require('./folderRouter')
 const taskRouter = require('./taskRouter')
+const accountsRouter = require('./accountsRouter')
 
 const routerApi = (app) => {
     const router = express.Router()
@@ -13,6 +14,7 @@ const routerApi = (app) => {
     router.use('/projects', proyectRouter)
     router.use('/folders', folderRouter)
     router.use('/tasks', taskRouter)
+    router.use('/accounts', accountsRouter)
 }
 
 module.exports = routerApi
