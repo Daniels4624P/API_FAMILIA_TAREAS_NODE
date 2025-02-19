42d8e13fba400f55318edfb0e33bfc01fa07ff39
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2025-02-19 13:25:16 -0500

    Update 20250219152237-unnamed-migration.js

--- db/migrations/20250219152237-unnamed-migration.js
+++ db/migrations/20250219152237-unnamed-migration.js
@@ -2,6 +2,7 @@
 
 const { ACCOUNTS_TABLE } = require('./../models/accountsModel')
 const { EXPENSES_TABLE } = require('./../models/expensesModel')
+const { INCOMES_TABLE } = require('./../models/incomesModel')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -21,10 +22,21 @@ module.exports = {
       },
       onDelete: 'SET NULL'
     })
+    await queryInterface.addColumn(INCOMES_TABLE, 'destino_id', {
+      allowNull: true,
+      type: Sequelize.DataTypes.INTEGER,
+      field: 'destino_id',
+      references: {
+        model: 'Accounts',
+        key: 'id'
+      },
+      onDelete: 'SET NULL'
+    })
   },
 
   async down (queryInterface, Sequelize) {
     await queryInterface.removeColumn(ACCOUNTS_TABLE, 'public')
     await queryInterface.removeColumn(EXPENSES_TABLE, 'destino_id')
+    await queryInterface.removeColumn(INCOMES_TABLE, 'destino_id')
   }
 };
