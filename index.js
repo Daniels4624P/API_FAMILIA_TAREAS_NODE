e3b9bdad5c711b259a228818b22006db68c17165
Daniels4624P <danielfelipesaenzbaquero@gmail.com>
2025-07-01 20:03:57 -0500

    Problema con el cors

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin: 'https://proyecto-familia-tareas-frontend.onrender.com'
+}))
 app.use(helmet())
 
 routerApi(app)
