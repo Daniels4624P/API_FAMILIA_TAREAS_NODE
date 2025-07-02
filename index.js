0a0bc3aa382320be8dfa180309da5d522dddde44
Daniels4624P <danielfelipesaenzbaquero@gmail.com>
2025-07-01 20:28:42 -0500

    Otra vez el cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://proyecto-familia-tareas-frontend.onrender.com'
+    origin: '*'
 }))
 app.use(helmet())
 
