436a6d51ede476318da92533318740ae3028c80a
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2025-07-03 15:56:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: 'https://proyecto-familia-tareas-frontend.onrender.com/', // Tu frontend
+  origin: 'https://proyecto-familia-tareas-frontend.onrender.com', // Tu frontend
   credentials: true, // Permitir cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
